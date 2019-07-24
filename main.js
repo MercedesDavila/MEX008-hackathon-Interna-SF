f3a0568f0607d3a0ab6293841a3f15dfd4b6e349
MercedesDavila <mmdavila@outlook.com>
2019-07-24 17:22:45 -0500

    Se agregó menú hamburguesa

---
+++ main.js
@@ -0,0 +1,8 @@
+//Functions sidenav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
