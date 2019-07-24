4b8966a6ef97684bec7db94c910ec65383e64fc7
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-07-24 18:40:05 -0500

    Creando funciones para navegar en paginas

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+//Const
+const enterHome = document.getElementById('enter');
+const registerButton = document.getElementById('register-button');
+
+//function open or close sections
+const showSection = id => document.getElementById(id).classList.remove('hide');
+const hideSection = id => document.getElementById(id).classList.add('hide');
+
+//functions to change sections
+const goingHome = () => {
+    hideSection('login');
+    showSection('HomePage')
+}
+
+const goingRegister = () => {
+    hideSection('login');
+    showSection('register')
+}
+
+enterHome.addEventListener('click', goingHome);
+registerButton.addEventListener('click', goingRegister);
