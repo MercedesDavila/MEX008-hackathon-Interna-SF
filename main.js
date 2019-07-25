1c40d7bcc0e9bc05f83242e4d4f311fef28a7d2c
LedaHuerta <ledamhuerta@gmail.com>
2019-07-25 04:56:35 -0500

    subiendo funcion de localstorage que guarda los datos de la seccion registro, aun no se utilizan

--- main.js
+++ main.js
@@ -17,6 +17,16 @@ const submitInfoNewUser = document.getElementById('accept');
 const submitInfoBudget = document.getElementById('accept-budget');
 const submitInfoSavings = document.getElementById('accept-savings');
 
+
+// declaring global variables to save user data
+// Register Section Data: Capturing Data
+let nameRegister = document.getElementById('name');
+let mailRegister = document.getElementById('e-mail');
+let passRegister = document.getElementById('pass');
+let pass2Register = document.getElementById('pass2');
+
+
+
 //function open or close sections
 const showSection = id => document.getElementById(id).classList.remove('hide');
 const hideSection = id => document.getElementById(id).classList.add('hide');
@@ -28,6 +38,7 @@ const savingName = () => {
     // print name
 
 //functions to change sections
+
 const goingHome = () => {
     hideSection('login');
     hideSection('register');
@@ -62,6 +73,32 @@ enterHome.addEventListener('click', goingHome);
 registerButton.addEventListener('click', goingRegister);
 budgetA.addEventListener('click', goingBudget);
 homeA.addEventListener('click', goingHome);
-submitInfoNewUser.addEventListener('click', goingBudget);
+// submitInfoNewUser.addEventListener('click', goingBudget);
 submitInfoBudget.addEventListener('click', goingSavings);
-submitInfoSavings.addEventListener('click', goingHome);
+
+// functions to save data user//
+let saveInfoNewUser = () => {
+    goingBudget();
+    //Data capture written in the inputs
+    nameRegister = document.getElementById('name').value;
+    mailRegister = document.getElementById('e-mail').value;
+    passRegister = document.getElementById('pass').value;
+    pass2Register = document.getElementById('pass2').value;
+    //saving data in localstorage
+    localStorage.setItem('usuario', nameRegister);
+    localStorage.setItem('mail', mailRegister);
+    localStorage.setItem('contraseña', passRegister);
+    localStorage.setItem('verificar-contraseña', pass2Register);
+    //clearinputs
+    document.getElementById('name').value = '';
+    document.getElementById('e-mail').value = '';
+    document.getElementById('pass').value = '';
+    document.getElementById('pass2').value = '';
+
+    // console.log(usuario, mail, contraseña, verificar - contraseña);
+}
+
+
+
+// events to save data user
+submitInfoNewUser.addEventListener('click', saveInfoNewUser);
